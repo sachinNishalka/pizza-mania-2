cb11b5982f4f4d0fd02fb061f6b50823a044f651
sachinNishalka <nishalkasachin@gmail.com>
2025-01-05 23:21:16 +0530

    working main

--- frontend/webapp-main/pizza-mania/src/App.jsx
+++ frontend/webapp-main/pizza-mania/src/App.jsx
@@ -1,49 +1,13 @@
-
-import Aboutfood from "./ui/Aboutfood";
-import Aboutus from "./ui/Aboutus";
-import Dilivery from "./ui/Dilivery";
-import FeaturesSection from "./ui/FeaturesSection";
-import FoodGrid from "./ui/Foodgrid";
-import Footer from "./ui/Footer";
-import NavBar from "./ui/NavBar";
-import OrganicFeatures from "./ui/OrganicFeatures";
-import RestaurantLanding from "./ui/RestaurantLanding";
-import Ceo from "./ui/ceo";
-import DeliveryBenefits from "./ui/DeliveryBenefits";
-// import ImageSlider from "./ui/ImageSlider";
 import Footer from './ui/Footer';
 import NavBar from './ui/NavBar';
-import PromoFSec from './ui/PromoFSec';
 import PopularFoodItems from './ui/Section1';
 import Section2 from './ui/Section2';
 import FoodMenuSection from './ui/Section3';
 import HeroSlider from './ui/Slider';
 
-
 export default function App() {
   return (
     <>
-
-    
-    
-     
-//       <div>Application Body</div>
-//       <FeaturesSection></FeaturesSection>
-//       <Aboutfood></Aboutfood>
-//       <Ceo></Ceo>
-//       {/* <ImageSlider></ImageSlider> */}
-//       <FoodGrid></FoodGrid>
-//       <Dilivery></Dilivery>
-//       <Aboutus></Aboutus>
-//       <RestaurantLanding></RestaurantLanding>
-//       <OrganicFeatures></OrganicFeatures>
-//       <DeliveryBenefits></DeliveryBenefits>
-//       <Footer></Footer>
-
-      
-     
-      
-
       <div className="h-screen">
         <NavBar></NavBar>
         <main>
@@ -54,7 +18,6 @@ export default function App() {
         </main>
         <Footer></Footer>
       </div>
-
     </>
   );
 }
